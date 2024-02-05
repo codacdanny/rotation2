731a7df5c45d06ad31890c355e2c5f36ff427f21
Chinemerem Daniel <danpresh302@gmail.com>
2024-02-05 16:02:45 +0100

    code clean up and began sign up UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
+import { ChakraProvider } from "@chakra-ui/react";
 function App() {
-  return <></>;
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
