bfacc14f2d475581ce92630c7f07a87128d5dd5b
Chinemerem Daniel <danpresh302@gmail.com>
2024-02-16 01:14:12 +0100

    building the pickpage

---
+++ src/Pages/PickPage.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const PickPage = () => {
+  return (
+    <Box
+      height="100svh"
+      background="linear-gradient(180deg, #E1D7F2 0%, rgba(107, 57, 189, 0.20) 53.96%, rgba(225, 215, 242, 0.00) 100%))"></Box>
+  );
+};
+
+export default PickPage;
