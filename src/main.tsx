e60a440c5c03f69442530198106fa3412b47e3c9
Chinemerem Daniel <danpresh302@gmail.com>
2024-02-17 23:47:48 +0100

    added lexend font

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <App />
       </Router>
---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/lexend";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `Lexend, sans-serif`,
+    body: `Lexend, sans-serif`,
+  },
+});
+
+export default theme;
