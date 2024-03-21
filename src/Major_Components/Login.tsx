9871de867e21ce97cbcc5672b18d8603c0da5739
Chinemerem Daniel <danpresh302@gmail.com>
2024-03-21 20:51:36 +0100

    fixed login

--- src/Major_Components/Login.tsx
+++ src/Major_Components/Login.tsx
@@ -1,8 +1,34 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import { Box, Flex, Heading, Input, Text } from "@chakra-ui/react";
 import AuthButton from "../Minor_Components/AuthButton";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const Login: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Send formData to the backend
+    console.log(formData);
+    // You can make a fetch request or use any library like Axios to send data to the backend here
+  };
+
   return (
     <Flex
       backgroundColor="#F7F7F7"
@@ -17,22 +43,37 @@ const Login = () => {
         fontWeight="500">
         Welcome Back
       </Heading>
-      <Flex flexDirection="column" gap="2rem">
-        <Input variant="flushed" placeholder="Email address" required />
-        <Input variant="flushed" placeholder="Password" required />
-        <Box
-          fontSize=".7rem"
-          fontWeight={200}
-          color="#24133F"
-          textAlign="right">
-          <Link to="#">forgot password?</Link>
-        </Box>
-      </Flex>
-      
-       
+      <form onSubmit={handleSubmit}>
+        <Flex flexDirection="column" gap="2rem">
+          <Input
+            variant="flushed"
+            placeholder="Email address"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <Input
+            variant="flushed"
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <Box
+            fontSize=".7rem"
+            fontWeight={200}
+            color="#24133F"
+            textAlign="right">
+            <Link to="#">forgot password?</Link>
+          </Box>
+        </Flex>
+        <Box textAlign="center">
           <AuthButton buttonText="Login" />
-       
-    
+        </Box>
+      </form>
       <Flex
         gap=".5rem"
         justifyContent="center"
