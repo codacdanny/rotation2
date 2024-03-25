da787c2c0bb3f45222462e0a0e1063f0182aebbf
Chinemerem Daniel <danpresh302@gmail.com>
2024-03-25 19:47:24 +0100

    removed context2

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { SocketProvider } from "./services/Context/SocketContext.tsx";
 import theme from "./theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SocketProvider>
-      <ChakraProvider theme={theme}>
-        <Router>
-          <App />
-        </Router>
-      </ChakraProvider>
-    </SocketProvider>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>
 );
