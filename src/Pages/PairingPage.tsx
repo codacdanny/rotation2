7f2d3a29d1bd0f906abdb7570dddd56937e3653d
Chinemerem Daniel <danpresh302@gmail.com>
2024-04-15 20:47:34 +0100

    added play game time checker

--- src/Pages/PairingPage.tsx
+++ src/Pages/PairingPage.tsx
@@ -68,95 +68,102 @@ const IconButtonSection: React.FC<{ icon: string; alt: string }> = ({
 );
 
 const PairingPage = ({ socket }) => {
-  const navigate = useNavigate();
-  const toast = useToast();
   const { userDetails } = useUser();
+  const navigate = useNavigate();
+  const balance = userDetails.rcBalance;
+  const toast = useToast();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const time = parseInt(queryParams.get("time"));
-  const balance = userDetails.rcBalance;
   console.log(time);
   const handlePlay = () => {
-    if (socket && balance >= 200) {
-      socket.emit("play", userDetails);
+    if (balance) {
+      if (socket && balance >= 200) {
+        socket.emit("play", userDetails);
 
-      navigate("/game");
-    } else {
-      toast({
-        title: "Error",
-        description: "Insufficient RC balance",
-        status: "error",
-        position: "top-right",
-        duration: 9000,
-        isClosable: true,
-      });
+        navigate("/game");
+      } else {
+        toast({
+          title: "Error",
+          description: "Insufficient RC balance",
+          status: "error",
+          position: "top-right",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
     }
   };
   return (
-    <Box
-      paddingY="2rem"
-      paddingX="1rem"
-      minHeight="100svh"
-      bgGradient="linear-gradient(180deg, #E1D7F2 0%, rgba(107, 57, 189, 0.20) 53.96%, rgba(225, 215, 242, 0.00) 100%)">
-      <WelcomeSection />
-      <Flex justifyContent="center" marginY="2rem">
-        <CircularProgressSection />
-      </Flex>
-      <Flex gap=".5rem" alignItems="center" justifyContent="center">
-        <User />
-        <BsArrowRight size="30px" />
-        <User />
-      </Flex>
-      <VStack gap="2rem" marginY="2rem">
-        <Text>
-          Please Wait patiently and be here atleast 3 minutes before the game...
-        </Text>
-        {/* && time <= 2  */}
-        {time >= 0 ? (
-          <Box textAlign="center" marginY="1rem" width="fit-content">
-            <Button
-              color="#fff"
-              bgColor="#24133F"
-              width="100%"
-              className="pulse circle orange"
-              colorScheme="violet"
-              onClick={handlePlay}>
-              Play Game
-            </Button>
-          </Box>
-        ) : (
-          <></>
-        )}
+    <>
+      {balance && (
+        <Box
+          paddingY="2rem"
+          paddingX="1rem"
+          minHeight="100svh"
+          bgGradient="linear-gradient(180deg, #E1D7F2 0%, rgba(107, 57, 189, 0.20) 53.96%, rgba(225, 215, 242, 0.00) 100%)">
+          <WelcomeSection />
+          <Flex justifyContent="center" marginY="2rem">
+            <CircularProgressSection />
+          </Flex>
+          <Flex gap=".5rem" alignItems="center" justifyContent="center">
+            <User />
+            <BsArrowRight size="30px" />
+            <User />
+          </Flex>
+          <VStack gap="2rem" marginY="2rem">
+            <Text>
+              Please Wait patiently and be here atleast 3 minutes before the
+              game...
+            </Text>
+            {/* && time <= 2  */}
+            {time >= 0 && time <= 2 ? (
+              <Box textAlign="center" marginY="1rem" width="fit-content">
+                <Button
+                  color="#fff"
+                  bgColor="#24133F"
+                  width="100%"
+                  className="pulse circle orange"
+                  colorScheme="violet"
+                  onClick={handlePlay}>
+                  Play Game
+                </Button>
+              </Box>
+            ) : (
+              <></>
+            )}
 
-        {time > 720 ? (
-          <Text textAlign="center" fontWeight="500">
-            You Missed The Game , Join Next Time
-          </Text>
-        ) : (
-          <Text textAlign="center" fontWeight="500">
-            Your game with User22a7 begins in{" "}
-            <span
-              style={{
-                color: "#06BCC1",
-              }}>
-              {time} minutes
-            </span>{" "}
-          </Text>
-        )}
+            {time > 720 ? (
+              <Text textAlign="center" fontWeight="500">
+                You Missed The Game , Join Next Time
+              </Text>
+            ) : (
+              <Text textAlign="center" fontWeight="500">
+                Your game with User22a7 begins in{" "}
+                <span
+                  style={{
+                    color: "#06BCC1",
+                  }}>
+                  {time} minutes
+                </span>{" "}
+              </Text>
+            )}
 
-        <Text textAlign="center" fontWeight="600" fontSize=".8rem">
-          The Play button appears 20 seconds before the game. so you can click
-          and join a room <br />
-          Also put your device in Landscape mode to get a good feel of the game
-          room
-        </Text>
-        <Text textAlign="center" fontWeight="300" fontSize=".8rem">
-          Do kindly note that any player who fails to start the game at the end
-          of the timer loses the game.
-        </Text>
-      </VStack>
-      <FlexSection />
-    </Box>
+            <Text textAlign="center" fontWeight="600" fontSize=".8rem">
+              The Play button appears 20 seconds before the game. so you can
+              click and join a room <br />
+              Also put your device in Landscape mode to get a good feel of the
+              game room
+            </Text>
+            <Text textAlign="center" fontWeight="300" fontSize=".8rem">
+              Do kindly note that any player who fails to start the game at the
+              end of the timer loses the game.
+            </Text>
+          </VStack>
+          <FlexSection />
+        </Box>
+      )}
+    </>
   );
 };
 
